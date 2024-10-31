02b93c5312eed8e27898beb9b8f06596d96a7462
ZYK006 <68289059+ZYK006@users.noreply.github.com>
2024-10-31 15:17:13 +0800

    Update rte_converter.py

--- preprocess/convert/converter/rte_converter.py
+++ preprocess/convert/converter/rte_converter.py
@@ -1,6 +1,5 @@
 import json
 
-
 class RTEConverter:
     def __init__(self, NAN="NAN", template_path='preprocess/template/rte_template.json'):
         template = json.load(open(template_path, 'r'))
@@ -8,8 +7,7 @@ class RTEConverter:
         self.relation_template = template['template']
         self.relation_int_out_format_en = {
             0:['{(Subject,Relation,Object)}', self.relation_convert_target0],
-            1:['{head entity is the relation of tail entity}', self.relation_convert_target1],
-            2:['{Relation：Subject,Object}', self.relation_convert_target2],
+            1:['{Relation：Subject,Object}', self.relation_convert_target1],
         }
         self.relation_int_out_format = self.relation_int_out_format_en
 
@@ -29,24 +27,9 @@ class RTEConverter:
         if len(output_text.replace(self.NAN, '').replace('\n', '').strip()) == 0:
             return self.NAN
         return output_text 
-    
+
 
     def relation_convert_target1(self, rels):
-        output_text = []
-        for rel in rels:
-            head = self.nan(rel['head'])
-            relation = self.nan(rel['relation'])
-            tail = self.nan(rel['tail'])
-            if head == "" or relation == "" or tail == "":
-                continue
-            output_text.append(f"{head} is the {relation} of {tail}")
-        output_text = '\n'.join(output_text)
-        if len(output_text.replace(self.NAN, '').replace('\n', '').strip()) == 0:
-            return self.NAN
-        return output_text
-
-
-    def relation_convert_target2(self, rels):
         output_text = []
         for rel in rels:
             head = self.nan(rel['head'])
@@ -61,9 +44,8 @@ class RTEConverter:
         return output_text 
     
 
-    def convert(self, record, rand1, rand2, s_schema=""):
+    def convert(self, record, rand1, rand2, s_schema1="", s_schema2=""):
         output_template = self.relation_int_out_format[rand2]
         output_text = output_template[1](record)
-        sinstruct = self.relation_template[str(rand1)].format(s_format=output_template[0], s_schema=s_schema)
+        sinstruct = self.relation_template[str(rand1)].format(s_format=output_template[0], s_schema1=s_schema1, s_schema2=s_schema2)
         return sinstruct, output_text
-
