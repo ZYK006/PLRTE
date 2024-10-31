71e4cd007168521a68d506771fb9ebd933ce5644
ZYK006 <68289059+ZYK006@users.noreply.github.com>
2024-10-31 15:17:52 +0800

    Update ner_converter.py

--- preprocess/convert/converter/ner_converter.py
+++ preprocess/convert/converter/ner_converter.py
@@ -5,7 +5,7 @@ class NERConverter:
         template = json.load(open(template_path, 'r'))
         self.entity_template = template['template']
         self.entity_int_out_format_en = {
-            0: ["{entity1,entity2}", self.entity_convert_target0]
+            0: ["{[entity1], [entity2]}", self.entity_convert_target0]
         }
         self.entity_int_out_format = self.entity_int_out_format_en
     
@@ -13,18 +13,13 @@ class NERConverter:
     def extract_entities(self, entities):
         return [entity["entity"] for entity in entities if entity["entity"].strip()]
 
-
     def entity_convert_target0(self, entities):
         extracted_entities = self.extract_entities(entities)
-        return ','.join(extracted_entities)
+        return ','.join([f'[{entity}]' for entity in extracted_entities])
 
 
-    def convert(self, record, rand1, rand2, s_schema=""):
+    def convert(self, record, rand1, rand2, s_schema1="", s_schema2=""):
         output_template = self.entity_int_out_format[rand2]
         output_text = output_template[1](record)
-        sinstruct = self.entity_template[str(rand1)].format(s_format=output_template[0], s_schema=s_schema)
+        sinstruct = self.entity_template[str(rand1)].format(s_format=output_template[0], s_schema=s_schema1)
         return sinstruct, output_text
-    
-
-
-
